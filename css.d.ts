df2091b40bd9295818eb2530784a4601854fc442
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-06-08 15:53:53 -0700

    feat: add typescript, convert nav.tsx

---
+++ css.d.ts
@@ -0,0 +1,13 @@
+declare module '*.scss' {
+  const css: { [key: string]: string };
+  export default css;
+}
+declare module '*.sass' {
+  const css: { [key: string]: string };
+  export default css;
+}
+declare module 'react-markup';
+declare module '*.webp';
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./Nav.module.scss";
 import OverlayTitle from "../../images/icons/overlay-title.png";
 
