3350e22de265345da18a60d4fbf13501afb2c6f3
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2021-06-10 21:02:30 -0700

    feat: make container for about links stats

--- src/pages/Market/Market.tsx
+++ src/pages/Market/Market.tsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+import { device } from '../../theme/theme';
 import About from "./About/About";
 import Links from "./Links/Links";
 import Stats from "./Stats/Stats";
@@ -6,12 +8,29 @@ import Technical from "./Technical/Technical";
 import Email from "./Email/Email";
 import Stakeholders from "./Stakeholders/Stakeholders";
 
+interface TopContainerProps {
+  maxWidth: number;
+}
+
+const TopContainer = styled.div<TopContainerProps>`
+  display: flex;
+  flex-direction: column;
+  max-width: ${(props) => props.maxWidth}px;
+
+  @media ${device.lg} {
+    flex-direction: row;
+    padding-left: 30px;
+  }
+
+`
 const Market: React.FC = () => {
   return (
     <div>
+    <TopContainer maxWidth={1400}>
       <About header="About"/>
       <Links header="Links"/>
       <Stats header="Stats"/>
+    </TopContainer>
       <Streams header="Streams"/>
       <Email header="Stay Updated"/>
       <Technical header="Technical"/>
