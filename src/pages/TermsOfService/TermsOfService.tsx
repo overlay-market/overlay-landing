6816e1669265ede55eae33003c079a65b8bf9019
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-09-05 16:35:50 -0700

    init basic Container for ToS

--- src/pages/TermsOfService/TermsOfService.tsx
+++ src/pages/TermsOfService/TermsOfService.tsx
@@ -1,5 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1000px;
+  margin: auto;
+`
+
 const TermsOfService = () => {
-  return <>Terms of Service</>
+  return <Container>Terms of Service</Container>
 }
 
 export default TermsOfService
