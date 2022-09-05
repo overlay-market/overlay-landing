5ea91a3e8518aed8eb161b81a19a6ddcfff01cd7
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-09-05 16:49:21 -0700

    Auditors section back up, add Spearbit w/ audit link

--- src/pages/Market/Auditors/Auditors.tsx
+++ src/pages/Market/Auditors/Auditors.tsx
@@ -1,36 +1,33 @@
-import { FeedCard, ListRow, Icon, Link } from '../../../components';
-import PeckshieldIcon from "../../../assets/icons/peckshield-logo.png";
+import {FeedCard, ListRow, Icon, Link} from '../../../components'
+import Spearbit_Logo from '../../../assets/icons/spearbit-logo.png'
 
 const data = [
-  { name: "Peckshield", link: "https://peckshield.com/en", icon: `${PeckshieldIcon}` }
-];
+  {
+    name: 'Spearbit',
+    link: 'https://github.com/overlay-market/v1-core/blob/main/audits/spearbit/audit.pdf',
+    icon: `${Spearbit_Logo}`,
+  },
+]
 
-const Auditors = ({
-  header
-}:{
-  header: string
-}) => {
+const Auditors = ({header}: {header: string}) => {
   return (
     <FeedCard header={header}>
-        {data.map((auditor, key) => (
-            <ListRow>
-              <Icon 
-                height={24} 
-                width={24} 
-                margin={'0 5px 0 0'} 
-                src={auditor.icon} 
-                alt={`${auditor.name} Icon`} 
-                />
-                <Link
-                    key={key}
-                    href={auditor.link}
-                    >
-                    {auditor.name}
-                </Link>
-            </ListRow>
-        ))}
+      {data.map((auditor, key) => (
+        <ListRow>
+          <Icon
+            height={24}
+            width={24}
+            margin={'0 5px 0 0'}
+            src={auditor.icon}
+            alt={`${auditor.name} Icon`}
+          />
+          <Link key={key} href={auditor.link}>
+            {auditor.name}
+          </Link>
+        </ListRow>
+      ))}
     </FeedCard>
   )
-};
+}
 
-export default Auditors;
+export default Auditors
--- src/pages/Market/Market.tsx
+++ src/pages/Market/Market.tsx
@@ -1,21 +1,21 @@
-import styled from 'styled-components';
-import { device } from '../../theme/theme';
-import About from "./About/About";
-import Links from "./Links/Links";
-import Stats from "./Stats/Stats";
-import Technical from "./Technical/Technical";
-import Stakeholders from "./Stakeholders/Stakeholders";
-// import Auditors from './Auditors/Auditors';
-import { CardContainer } from "../../components/FeedCard/FeedCard";
+import styled from 'styled-components'
+import {device} from '../../theme/theme'
+import About from './About/About'
+import Links from './Links/Links'
+import Stats from './Stats/Stats'
+import Technical from './Technical/Technical'
+import Stakeholders from './Stakeholders/Stakeholders'
+import Auditors from './Auditors/Auditors'
+import {CardContainer} from '../../components/FeedCard/FeedCard'
 
 interface ContainerProps {
-  maxWidth?: number;
+  maxWidth?: number
 }
 
 const Container = styled.div<ContainerProps>`
   display: flex;
   flex-direction: column;
-  max-width: ${(props) => props.maxWidth}px;
+  max-width: ${props => props.maxWidth}px;
 
   @media ${device.lg} {
     flex-direction: row;
@@ -26,19 +26,19 @@ const Market: React.FC = () => {
   return (
     <>
       <Container>
-        <About header="Overlay"/>
-        <Links header="Links"/>
-        <Stats header="Stats"/>
+        <About header="Overlay" />
+        <Links header="Links" />
+        <Stats header="Stats" />
       </Container>
-        {/* <Streams header="Streams"/> */}
+      {/* <Streams header="Streams"/> */}
       <Container>
-        <Technical header="Technical"/>
-        <Stakeholders header="Stakeholders"/>
-        {/* <Auditors header="Auditors" /> */}
+        <Technical header="Technical" />
+        <Stakeholders header="Stakeholders" />
+        <Auditors header="Auditors" />
         <CardContainer />
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default Market;
+export default Market
