948a63610ab644f83508c47ce7254c0328e51997
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2022-11-01 14:43:11 -0700

    add immunfi logo

--- src/pages/Market/Audits/Audits.tsx
+++ src/pages/Market/Audits/Audits.tsx
@@ -1,6 +1,7 @@
 import {FeedCard, ListRow, Icon, Link} from '../../../components'
 import Spearbit_Logo from '../../../assets/icons/spearbit-logo.png'
 import LeastAuthority_Logo from '../../../assets/icons/leastauthority-logo.png'
+import Immunefi_Logo from '../../../assets/icons/immunefi-icon.png'
 
 const data = [
   {
@@ -12,6 +13,11 @@ const data = [
     name: 'Spearbit',
     link: 'https://github.com/overlay-market/v1-core/blob/main/audits/spearbit/audit.pdf',
     icon: `${Spearbit_Logo}`,
+  },
+  {
+    name: 'Immunefi',
+    link: 'https://immunefi.com/bounty/overlay/',
+    icon: `${Immunefi_Logo}`,
   },
 ]
 
