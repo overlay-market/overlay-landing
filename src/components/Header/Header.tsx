d6d753e69142589dc6e7c43c32f32562d5e10cf1
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-03 15:17:25 -0800

    skeleton Header component with Logo, Links, Buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,14 +9,10 @@ export const HeaderContainer = styled.div`
 
 export const LogoContainer = styled.div`
   display: flex;
+  cursor: pointer;
 `
 
 const LinksContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: row;
 `
@@ -29,15 +25,41 @@ const Link = styled.a`
   cursor: pointer;
 `
 
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const TokenFaucetButton = styled.button`
+  background: transparent;
+  border: none;
+  box-shadow: none;
+  padding: 8px 16px;
+  margin-right: 48px;
+`
+
+const AppButton = styled.button`
+  background: #71ceff;
+  border-radius: 5px;
+  border: none;
+  box-shadow: none;
+  padding: 8px 16px;
+`
+
 const Header = () => {
   return (
     <HeaderContainer>
-      <LogoContainer></LogoContainer>
+      <LogoContainer>Overlay</LogoContainer>
+
       <LinksContainer>
         <Link>Governance</Link>
         <Link>Documentation</Link>
       </LinksContainer>
-      <ButtonsContainer></ButtonsContainer>
+
+      <ButtonsContainer>
+        <TokenFaucetButton>Get OVL</TokenFaucetButton>
+        <AppButton>Launch App</AppButton>
+      </ButtonsContainer>
     </HeaderContainer>
   )
 }
