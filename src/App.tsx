0f30a417323fd0b8d0b46d59d42e861e95d4962e
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-04 14:29:39 -0800

    add Value Proposition text props on left side of split

--- src/App.tsx
+++ src/App.tsx
@@ -16,11 +16,37 @@ const PrimaryViewContainer = styled.div``
 
 const SecondaryViewContainer = styled.div``
 
+const Title = styled.div`
+  font-size: 36px;
+  font-weight: bold;
+  color: black;
+`
+
+const Description = styled.div`
+  font-size: 18px;
+  font-weight: regular;
+  color: #7b7b7b;
+`
+
+const LightArrowButton = styled.button`
+  border: 1px solid #71ceff;
+  background: white;
+  box-shadow: none;
+  outline: none;
+`
+
 const ValueProposition = () => {
   return (
     <MainContainer>
       <DesktopSplitViewContainer>
-        <PrimaryViewContainer></PrimaryViewContainer>
+        <PrimaryViewContainer>
+          <Title>Say Goodbye to Liquidity Pools and Counter Parties</Title>
+          <Description>
+            Overlay is built from the ground up with OVL, a cryptocurrency that longs/shorts DeFi
+            data streams and much, much more.
+          </Description>
+          <LightArrowButton>Learn More</LightArrowButton>
+        </PrimaryViewContainer>
         <SecondaryViewContainer></SecondaryViewContainer>
       </DesktopSplitViewContainer>
     </MainContainer>
