566e8403d2de1e9f3859fcd553e7731880546507
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-04 11:01:21 -0800

    initialize presentational Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import {Route, Switch} from 'react-router-dom'
+import styled from 'styled-components'
 import {Market, TermsOfService} from './pages'
 import {Nav} from './components'
 import './App.scss'
+
+const HeroContainer = styled.div`
+  min-height: 625px;
+`
+
+const SplitViewContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const PrimaryViewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const SecondaryViewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Hero = () => {
+  return (
+    <HeroContainer>
+      <SplitViewContainer>
+        <PrimaryViewContainer></PrimaryViewContainer>
+        <SecondaryViewContainer></SecondaryViewContainer>
+      </SplitViewContainer>
+    </HeroContainer>
+  )
+}
 
 const App: React.FC = () => {
   return (
@@ -9,7 +40,9 @@ const App: React.FC = () => {
       <Nav />
       <Switch>
         {/* TO DO: add Landing Page components in / route */}
-        <Route path="/" exact></Route>
+        <Route path="/" exact>
+          <Hero />
+        </Route>
         <Route exact strict path="/markets" component={Market} />
         <Route exact strict path="/ToS" component={TermsOfService} />
       </Switch>
