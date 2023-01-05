9c65731524f1170fd7cf2ecbb535f09d2b2320ac
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-04 16:01:11 -0800

    style: news brands display on desktop

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components'
+import NewsBrandsLogos from '../../assets/images/news-brands-desktop.png'
+import CoindeskLogo from '../../assets/images/coindesk-brand-logo.png'
+import BloombergLogo from '../../assets/images/bloomberg-brand-logo.png'
+import MessariLogo from '../../assets/images/messari-brand-logo.png'
 
 const MainContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   padding: 45px 0;
 `
@@ -10,20 +15,32 @@ const Title = styled.div`
   font-size: 24px;
   font-weight: bold;
   text-align: center;
+  margin-bottom: 24px;
 `
 
 const NewsPlatformContainer = styled.div`
   display: flex;
+  padding: 40px 0;
+  margin: auto;
+  width: 1100px;
+  max-width: 1100px;
   flex-direction: row;
+  justify-content: center;
   border-top: 0.5px solid #232323;
   border-bottom: 0.5px solid #232323;
+`
+
+const BrandLogo = styled.img`
+  height: 70px;
 `
 
 const News = () => {
   return (
     <MainContainer>
       <Title>Overlay in the news</Title>
-      <NewsPlatformContainer></NewsPlatformContainer>
+      <NewsPlatformContainer>
+        <BrandLogo src={NewsBrandsLogos} />
+      </NewsPlatformContainer>
     </MainContainer>
   )
 }
