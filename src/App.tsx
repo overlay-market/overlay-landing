38962bcadc5c1a966034e03a4ccdcd95bd10e00a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-06 15:17:58 -0800

    init Markets component with basic children components

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,59 @@ import MediaCarousel from './components/MediaCarousel/MediaCarousel'
 import Footer from './components/Footer/Footer'
 import './App.scss'
 
+const MainContainer = styled.div`
+  display: flex;
+  max-width: 1100px;
+  margin: auto;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`
+
+const Title = styled.div`
+  font-size: 36px;
+  font-weight: bold;
+  max-width: 450px;
+`
+
+const Description = styled.div`
+  font-size: 18px;
+  color: #7b7b7b;
+  max-width: 650px;
+`
+
+const ExploreButton = styled.button`
+  background: #71CEFF;
+  font-weight: 18px;
+  color: white;
+  border-radius: 10px;
+  outline: none;
+  box-shadow; none;
+  border: none;
+  padding: 12px 24px;
+`
+
+const Markets = () => {
+  return (
+    <MainContainer>
+      <HeaderContainer>
+        <div>
+          <Title>Discover a wide world of new markets</Title>
+          <Description>
+            Overlay can spin up markets on nearly any scalar data. From DeFi to the real world,
+            nothing will be illiquid again.
+          </Description>
+        </div>
+        <div>
+          <ExploreButton>Explore</ExploreButton>
+        </div>
+      </HeaderContainer>
+    </MainContainer>
+  )
+}
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -22,6 +75,7 @@ const App: React.FC = () => {
           <Investors />
           <ValueProposition />
           <News />
+          <Markets />
           <Audits />
           <Contributing />
           <MediaCarousel />
