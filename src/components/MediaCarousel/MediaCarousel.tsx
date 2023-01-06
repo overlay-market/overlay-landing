89686e5d9879759e2527684f10615f78dcbb7688
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-06 14:30:03 -0800

    rendering latest OverlayProtocol twitter timeline

--- src/components/MediaCarousel/MediaCarousel.tsx
+++ src/components/MediaCarousel/MediaCarousel.tsx
@@ -1,9 +1,18 @@
 import styled from 'styled-components'
+import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
 const MainContainer = styled.div``
 
 const MediaCarousel = () => {
-  return <MainContainer></MainContainer>
+  return (
+    <MainContainer>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="OverlayProtocol"
+        options={{height: 400}}
+      />
+    </MainContainer>
+  )
 }
 
 export default MediaCarousel
