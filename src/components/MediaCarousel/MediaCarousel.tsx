69404b5efe3f4edd4b60a048dcbf835ac4731b8a
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-06 14:39:00 -0800

    style: MediaCarousel twitter embedd placement

--- src/components/MediaCarousel/MediaCarousel.tsx
+++ src/components/MediaCarousel/MediaCarousel.tsx
@@ -1,16 +1,47 @@
 import styled from 'styled-components'
 import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
-const MainContainer = styled.div``
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 1100px;
+  width: 100%;
+  margin: auto;
+  padding: 45px 0;
+`
+
+const Title = styled.div`
+  font-size: 54px;
+  font-weight: bold;
+  width: 100%;
+  text-align: left;
+  border-bottom: 1px solid #232323;
+  padding-bottom: 30px;
+`
+
+const CarouselContainer = styled.div`
+  display: flex;
+  padding-top: 45px;
+`
+
+const TwitterEmbedContainer = styled.div`
+  width: 350px;
+  height: 700px;
+`
 
 const MediaCarousel = () => {
   return (
     <MainContainer>
-      <TwitterTimelineEmbed
-        sourceType="profile"
-        screenName="OverlayProtocol"
-        options={{height: 400}}
-      />
+      <Title>Latest Updates</Title>
+      <CarouselContainer>
+        <TwitterEmbedContainer>
+          <TwitterTimelineEmbed
+            sourceType="profile"
+            screenName="OverlayProtocol"
+            options={{height: 700}}
+          />
+        </TwitterEmbedContainer>
+      </CarouselContainer>
     </MainContainer>
   )
 }
