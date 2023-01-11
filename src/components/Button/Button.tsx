b2dce7ef510df42bab9619758263d0b54f6649f4
Jonah Lin <57741810+linjonah@users.noreply.github.com>
2023-01-10 16:48:52 -0800

    add optional href param to StyledCTAButton

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,8 +3,13 @@ import './Button.css'
 
 interface StyledCTAButtonProps {
   children: React.ReactNode | string
+  href?: string
 }
 
-export const StyledCTAButton = ({children}: StyledCTAButtonProps) => {
-  return <button className="cta-button glow-on-hover">{children}</button>
+export const StyledCTAButton = ({children, href}: StyledCTAButtonProps) => {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer">
+      <button className="cta-button glow-on-hover">{children}</button>
+    </a>
+  )
 }
