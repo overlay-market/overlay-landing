478c363c99bf7116c5c13710d131ec4ee545f873
Tomas C Imach <43709975+TomasCImach@users.noreply.github.com>
2023-08-15 15:44:25 -0300

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -5,7 +5,7 @@ export const GENERAL_LINKS = {
   COMMUNITY_DOCS:
     'https://flash-asp-362.notion.site/Overlay-Protocol-29da60fd11e9436ea69e2a5e0db28a74',
   WHITEPAPER_V1:
-    'https://planckcat.mypinata.cloud/ipfs/QmVMX7DH8Kh22kxMyDFGUJcw1a3irNPvyZBtAogkyJYJEv',
+    'https://gateway.pinata.cloud/ipfs/QmVMX7DH8Kh22kxMyDFGUJcw1a3irNPvyZBtAogkyJYJEv',
   DISCORD: 'https://discord.gg/FgNhRNtQYv',
   TWITTER: 'https://twitter.com/OverlayProtocol',
   MIRROR: 'https://mirror.xyz/0x7999C7f0b9f2259434b7aD130bBe36723a49E14e',
