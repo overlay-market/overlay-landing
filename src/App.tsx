23e99a0a6c3827eb510f3b73b0e9cda46963ed24
AndresImach <andyci435@gmail.com>
2023-11-02 11:39:34 -0400

    Implementing Cookie-consent and style for it.

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,43 @@ import Audits from './components/Audits/Audits'
 import Contributing from './components/Contributing/Contributing'
 import MediaCarousel from './components/MediaCarousel/MediaCarousel'
 import Footer from './components/Footer/Footer'
+import { cookie3Analytics } from "@cookie3/analytics";
+import { useEffect } from 'react';
+import CookieConsent from 'react-cookie-consent'
 
 const App: React.FC = () => {
+  
+  useEffect(() => {
+    var gaProperty: string = 'G-QJR084KXFT'
+    var disableStr: string = 'ga-disable-' + gaProperty
+
+    const analytics = cookie3Analytics({"siteId":260,
+    disabled: document.cookie.indexOf(disableStr + '=true') > -1,});
+
+    analytics.trackPageView();
+  }, [])
+
   return (
     <div>
       <Switch>
         <Route path="/" exact>
+        <CookieConsent
+          enableDeclineButton
+          onDecline={() => {
+            alert("nay!");
+          }}
+          declineButtonText="Decline"
+          declineButtonStyle={{fontSize: "13px", padding: "16px, 36px, 16px, 36px", height: "40px", width: "116px", fontWeight: "700", background: "#ffffff00", border: "1px solid", borderRadius: "8px", borderColor: "#E5F6FF"}}
+          location="bottom"
+          buttonText="Allow"
+          cookieName="myAwesomeCookieName2"
+          style={{ background: "#2B373B" }}
+          buttonStyle={{ color: "white", fontSize: "13px", background: "#53B1F9", borderRadius: "8px", padding: "16px, 36px, 16px, 36px", height: "40px", width: "116px", fontWeight: "700" }}
+          expires={150}
+        >
+          This website uses cookies to enhance the user experience.{" "}
+          View our <a href="https://overlay.market/#/tos" style={{ color: "#ffffff" }}>TOS</a> to learn more.
+        </CookieConsent>
           <Nav />
           <Hero />
           <Investors />
