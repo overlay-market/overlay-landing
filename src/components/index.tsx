de189a568d98e7c778a022eeb80e42827954e31e
AndresImach <andyci435@gmail.com>
2025-08-12 10:15:21 -0300

    Add CountdownBanner component and integrate it into the App layout

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import Nav from './Header/Header'
 import {Link} from './Link/Link'
 import {Icon} from './Icon/Icon'
+import CountdownBanner from './CountdownBanner/CountdownBanner'
 
-export {Nav, Icon, Link}
+export {Nav, Icon, Link, CountdownBanner}
